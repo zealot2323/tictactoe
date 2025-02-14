6cf972dea67d06a7e05b358b566c4d23d770a34c
Eric Pratt <zealot2323@gmail.com>
2025-02-14 17:35:19 +0100

    Added remaining logic and styled page

--- tictactoe.js
+++ tictactoe.js
@@ -2,10 +2,103 @@ function createPlayer(name, symbol) {
     return { name, symbol };
 }
 
-function createBoard() {
-    let turn = 0;
-    let winner = null;
-    let board = new Array(9).fill(undefined);
+function manageBoard() {
+
+    const checkWin = function(board, turn) {
+        for (let [a, b, c] of winningCombos) {
+            if (board[a] && board[a] === board[a] && board[a] === board[b] && board[a] === board[c]) {
+                highlightWin([a, b, c]);
+                return board[a];
+            }
+        }
+        if (turn === 9) {
+            return "Tie";
+        } else {
+            return null;
+        }
+    }
+    
+    const updateBoard = function(board, event) {
+        let i = event.target.dataset.index;
+        if (board[i] === undefined && !game.winner) {
+            board[i] = game.currentPlayer;
+            updateSpace(i, game.currentPlayer);
+
+            game.turn++;
+
+            game.winner = checkWin(board, game.turn);
+            if (game.winner) {
+                updateWin(game.winner);
+                return
+            };
+            
+            game.currentPlayer = game.currentPlayer === "X" ? "O" : "X";
+
+            console.log(game.xPlayer);
+            console.log(game.oPlayer);
+            if (game.currentPlayer === "X" && game.xPlayer) {
+                updateState(game.xPlayer);
+            } else if (game.currentPlayer === "O" && game.oPlayer) {
+                updateState(game.oPlayer);
+            } else {
+                updateState(game.currentPlayer);
+            }
+            
+
+        } 
+    };
+
+    const updateSpace = function(index, sym) {
+        const spaceUpdate = document.querySelector(`[data-index="${index}"]`);
+        spaceUpdate.textContent = sym;
+    }
+
+    const updateState = function(sym) {
+        const stateSpace = document.querySelector(`.state`);
+        stateSpace.textContent = `It's ${sym}'s turn!`;
+    }
+
+    const updateWin = function(sym) {
+        const stateSpace = document.querySelector(`.state`);
+        if (sym === "Tie") {
+            stateSpace.textContent = `Tie!`;
+        } else {
+            if (sym === "X" && game.xPlayer) {
+                stateSpace.textContent = `${game.xPlayer} wins!`;
+            } else if (sym === "O" && game.oPlayer) {
+                stateSpace.textContent = `${game.oPlayer} wins!`;
+            } else {
+                stateSpace.textContent = `Player ${sym} wins!`;
+            }
+        };
+    }
+
+    const highlightWin = function([a, b, c]) {
+        const winningIndexes = [a, b, c];
+        winningIndexes.forEach( index => {
+            let winSpace = document.querySelector(`[data-index="${index}"]`);
+            winSpace.classList.add('win');
+        }); 
+    }
+
+    const firstPlayer = function() {
+        return Math.random() < 0.5 ? "X" : "O"; 
+    }
+
+    const resetBoard = function(board, ) {
+        game.currentPlayer = firstPlayer();
+        updateState(game.currentPlayer);
+        board.forEach((item, index) => {
+            const space = document.querySelector(`[data-index="${index}"]`);
+            space.textContent = "";
+            if(space.classList.contains('win')) {
+                space.classList.remove('win');
+            }
+        });
+        board.fill(undefined);
+        game.winner = null;
+        game.turn = 0;
+    }
 
     const winningCombos = [
         [0, 1, 2], // Top row
@@ -18,58 +111,39 @@ function createBoard() {
         [2, 4, 6]  // Diagonal from top-right to bottom-left
       ];
 
-    const checkWin = function(board) {
-        for (let [a, b, c] of winningCombos) {
-            if (board[a] && board[a] === board[a] && board[a] === board[b] && board[a] === board[c]) {
-                return board[a];
-            }
-        }
-        return null;
-    }
-    
-    const updateBoard = function(i, sym) {
-        board[i] = sym;
-        turn++;
-        console.log(turn);
-
-        winner = checkWin(board);
-        if (winner) {
-            console.log(winner);
-            return winner;
-        }
-        
-        return board;
-    };
-
-    const resetBoard = function() {
-        board.fill(undefined);
-        winner = null;
-        turn = 0;
-    }
-
-    return { board, turn, updateBoard, resetBoard };
+    return { updateBoard, resetBoard, updateState, firstPlayer };
 }
 
 const game = (function () {
-    const { board, turn, updateBoard, resetBoard } = createBoard(); //can remove board and turn maybe?
+    const { updateBoard, resetBoard, updateState, firstPlayer } = manageBoard();
+
+    let turn = 0;
+    let winner = null;
+    let currentPlayer = firstPlayer();
+    updateState(currentPlayer);
+    let xPlayer = null;
+    let oPlayer = null;
+    let board = new Array(9).fill(undefined);
+
+    
     
     const reset = document.querySelector('.reset button');
     reset.addEventListener('click', (e) => {
-        resetBoard();
+        resetBoard(board);
     });
 
     const playerXSubmit = document.querySelector('#x');
-    reset.addEventListener('click', (e) => {
+    playerXSubmit.addEventListener('click', (e) => {
         let playerX = document.getElementById("playerXName").value;
-        createPlayer(playerX, "X");
-        //updatescreen
+        game.xPlayer = createPlayer(playerX, "X").name;
+        document.getElementById("playerXName").value = "";
     });
 
     const playerOSubmit = document.querySelector('#o');
-    reset.addEventListener('click', (e) => {
+    playerOSubmit.addEventListener('click', (e) => {
         let playerO = document.getElementById("playerOName").value;
-        createPlayer(playerO, "O");
-        //updatescreen
+        game.oPlayer = createPlayer(playerO, "O").name;
+        document.getElementById("playerOName").value = "";
     });
 
     const gameBoard = document.querySelector('.board');
@@ -79,25 +153,11 @@ const game = (function () {
         space.setAttribute('data-index', index);
 
         space.addEventListener('click', (e) => {
-            updateBoard(e);
+            updateBoard(board, e);
         });
 
         gameBoard.appendChild(space);
     });
 
-    return {board, turn, updateBoard, resetBoard};
+    return {board, turn, currentPlayer, winner, xPlayer, oPlayer};
 })();
-
-console.log(game);
-game.updateBoard(1, "X");
-console.log(game.board);
-game.updateBoard(2, "X");
-console.log(game.board);
-console.log(game.updateBoard(5, "X"));
-game.updateBoard(0, "X");
-console.log(game.board);
-
-
-//check every other to see if it's the right turn
-//check the array if it has a value
-//screen updater
